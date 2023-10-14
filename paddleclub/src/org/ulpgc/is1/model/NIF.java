d13e1c91f9f2994baf6ed9ae9d950105ac7d0a4f
Alejandro Del Toro <aadeltoroo@gmail.com>
2023-10-14 12:13:35 +0100

    TODO done(class NIF created)

---
+++ paddleclub/src/org/ulpgc/is1/model/NIF.java
@@ -0,0 +1,21 @@
+package org.ulpgc.is1.model;
+
+public class NIF {
+    String number;
+
+    public NIF(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void isValid(){
+         //TODO terminar metodo
+    }
+}
