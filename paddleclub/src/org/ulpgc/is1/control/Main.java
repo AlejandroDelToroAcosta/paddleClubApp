8787332bbfaaaca8d51fb256224e26b9c6147158
Alejandro Del Toro <aadeltoroo@gmail.com>
2023-10-22 17:58:46 +0100

    final project version

--- paddleclub/src/org/ulpgc/is1/control/Main.java
+++ paddleclub/src/org/ulpgc/is1/control/Main.java
@@ -15,21 +15,29 @@ public class Main {
     }
 
     private static void init(PaddleManager paddleManager) {
-        ArrayList<Reservation> reservationList = new ArrayList<>();
-        NIF nif1 = new NIF("23056789A");
-        NIF nif2 = new NIF("12SD34TT4");
+         ArrayList<Reservation> reservationList = paddleManager.reserve();
 
-        Court court1 = new Court("Pista 1", 20, CourtType.SlowCourt, null);
-        Court court2 = new Court("Pista 2", 20, CourtType.FastCourt, null);
+        //. Crear dos clientes. El primer cliente será miembro del club de paddle.
+        //El segundo cliente tendrá el NIF: “12SD34TT4”.
 
-        Customer customer1 = new Customer("Francisco", "Navarro", nif1, reservationList);
-        Customer customer2 = new Customer("Juan", "Lebrón", nif2, reservationList);
+        paddleManager.addCustomer("Francisco", "Navarro", "12345678A",reservationList);
+        paddleManager.addCustomer("Juan", "Lebrón", "12SD34TT4", reservationList);
+
+        //Crear dos pistas de paddle, una será de tipo rápido y otra de tipo lento.
+        paddleManager.addCourt("Pista 1", 20, CourtType.SlowCourt, reservationList);
+        paddleManager.addCourt("Pista 2", 25, CourtType.FastCourt, reservationList);
+
+        Court court1 = paddleManager.getCourtList(0);
+        Court court2 = paddleManager.getCourtList(1);
+        Customer customer1 = paddleManager.getCustomerList(0);
+        Customer customer2 = paddleManager.getCustomerList(1);
 
         Date date1 = new Date(123, 9, 3, 15, 30);
         Date date2 = new Date(123, 6, 27, 16, 0);
 
-        Reservation firstReservation = new Reservation(12345, date1, court1, customer1);
-        Reservation secondReservation = new Reservation(12345, date2, court2, customer2);
+        Reservation firstReservation = new Reservation(0, date1, court1, customer1);
+        Reservation secondReservation = new Reservation(0, date2, court2, customer2);
+
 
         reservationList.add(firstReservation);
         reservationList.add(secondReservation);
@@ -40,38 +48,55 @@ public class Main {
         customer1.setReservations(reservationList);
         customer2.setReservations(reservationList);
 
-        //
+        //Imprimir por pantalla los NIF’s válidos de los clientes del club.
         printNIF(customer1);
         printNIF(customer2);
 
+        //Imprimir por pantalla los datos del primer cliente.
         printCustomerData(customer1, firstReservation);
 
-
+        //Imprimir por pantalla los datos del segundo cliente.
         printCustomerData(customer2, secondReservation);
 
+        //Imprimir por pantalla los datos de la segunda pista.
         printCourtData(court2, reservationList, 1);
 
         ///Crear una reserva para el primer cliente en la segunda pista
 
         Date reservationDate = new Date(123, 9, 5, 14, 0);
-        Reservation newReservation = new Reservation(6789, reservationDate, court2, customer1);
+        Reservation newReservation = new Reservation(0, reservationDate, court2, customer1);
 
-        ArrayList<Reservation> court2Reservations = court2.getReservations();
-        court2Reservations.add(newReservation);
 
-        court2.setReservations(court2Reservations);
+        if (!reservationExists(newReservation, reservationList)) {
+            reservationList.add(newReservation);
+            court2.setReservations(reservationList);
+            customer1.setReservations(reservationList);
 
-        ArrayList<Reservation> customer1Reservations = customer1.getReservations();
-        customer1Reservations.add(newReservation);
+            System.out.println("Reserva creada para el Cliente 1 en la Segunda Pista:");
+            System.out.println("ID de Reserva: " + newReservation.getId());
+            System.out.println("Fecha y hora de reserva: " + newReservation.getDate());
+            System.out.println("Pista: " + newReservation.getCourt().getName());
+            System.out.println("Cliente: " + newReservation.getCustomer().getName());
+            System.out.println("- - - -");
+        } else {
+            System.out.println("La reserva ya existe en la lista de reservas.");
+        }
 
-        customer1.setReservations(customer1Reservations);
+        //Borrar el segundo cliente
 
-        System.out.println("Reserva creada para el Cliente 1 en la Segunda Pista:");
-        System.out.println("ID de Reserva: " + newReservation.getId());
-        System.out.println("Fecha y hora de reserva: " + newReservation.getDate());
-        System.out.println("Pista: " + newReservation.getCourt().getName());
-        System.out.println("Cliente: " + newReservation.getCustomer().getName());
+        paddleManager.removeCustomer(customer2);
 
+        System.out.println(paddleManager.customerList);
+        System.out.println("Cliente eliminado");
+        System.out.println("- - - -");
+
+        //Imprimir por pantalla el número de clientes del club
+
+        int countedClients = paddleManager.count();
+        System.out.println("Número de clientes :" + countedClients);
+        System.out.println("- - - -");
+
+        printAllReservations(reservationList);
     }
 
     private static void printNIF(Customer customer) {
@@ -84,7 +109,6 @@ public class Main {
         }
     }
 
-    //
     private static void printCustomerData(Customer customer, Reservation reservation) {
         System.out.println("Datos del cliente: {");
         System.out.println("Nombre: " + customer.getName());
@@ -99,7 +123,6 @@ public class Main {
         System.out.println("Precio: " + court.getPrice());
         System.out.println("Tipo: " + court.getType());
 
-
         System.out.println("Reservas de la Segunda Pista:");
         Reservation secondReservation = reservationList.get(index);
 
@@ -108,11 +131,39 @@ public class Main {
         System.out.println("Cliente: " + secondReservation.getCustomer().getName());
         System.out.println("- - - -");
 
-
+    }
+    private static boolean reservationExists(Reservation reservation, ArrayList<Reservation> reservationList) {
+        for (Reservation existingReservation : reservationList) {
+            if (existingReservation.getId() == reservation.getId()) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
+    private static void printAllReservations(ArrayList<Reservation> reservationList) {
+        System.out.println("Reservas realizadas:");
+        int extraprice = 5;
+        Extra extra = new Extra(extraprice) {
+            @Override
+            public String getName() {
+                return null;
+            }
+        };
 
+        for (Reservation reservation : reservationList) {
+            Customer customer = reservation.getCustomer();
+            Court court = reservation.getCourt();
+            Date date = reservation.getDate();
+            int cost = reservation.price(extra, court) ;
 
+            System.out.println("Cliente: " + customer.getName() + " " + customer.getSurname());
+            System.out.println("Pista: " + court.getName());
+            System.out.println("Fecha: " + date);
+            System.out.println("Coste de la reserva: " + cost);
+            System.out.println("- - - -");
+        }
+    }
 
 }
--- paddleclub/src/org/ulpgc/is1/control/PaddleManager.java
+++ paddleclub/src/org/ulpgc/is1/control/PaddleManager.java
@@ -4,23 +4,25 @@ import org.ulpgc.is1.model.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class PaddleManager {
 
     public List<Customer> customerList;
     public List<Court> courtList;
+    public List<Reservation> reservationsList;
 
     public PaddleManager(){
         customerList = new ArrayList<>();
         courtList = new ArrayList<>();
+        reservationsList = new ArrayList<>();
     }
     public void addCustomer(String name, String surname, String nifNumber, ArrayList<Reservation> reservations) {
         NIF nif = new NIF(nifNumber);
         Customer customer;
         customer = new Customer(name, surname, nif, reservations);
-
-        customerList.add(customer);
+        if (!customerList.contains(customer)) customerList.add(customer);
 
     }
     public Customer getCustomerList(int index){
@@ -29,10 +31,27 @@ public class PaddleManager {
 
     public void addCourt(String name, int price, CourtType type, ArrayList<Reservation> reservations){
         Court newCourt = new Court(name, price, type, reservations);
-        courtList.add(newCourt);
+        if (!courtList.contains(newCourt)) courtList.add(newCourt);
     }
+
+    public ArrayList<Reservation> reserve(){
+        ArrayList<Reservation> reservationList = new ArrayList<>();
+        return reservationList;
+    }
+
 
     public Court getCourtList(int index){
         return courtList.get(index);
     }
+
+
+    public void removeCustomer(Customer customer){
+        customerList.remove(customer);
+
+    }
+
+    public int count(){
+        return customerList.size();
+    }
+
 }
